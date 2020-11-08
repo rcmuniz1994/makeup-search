05e2d71522552af441cb5ec92cc71743f4e2d797
Rodrigo Muniz <rcmuniz51@gmail.com>
2020-11-08 17:03:54 -0300

    feature: added page title

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import InputGroup from 'react-bootstrap/InputGroup';
 import FormControl from 'react-bootstrap/FormControl';
-import SearchIcon from '@material-ui/icons/Search';
 import ProductsTable from './components/ProductsTable';
+import Title from './components/Title';
+import SearchIcon from '@material-ui/icons/Search';
 import './App.css';
 
 function App() {
-  const [products, setProducts] = React.useState([]);
-  const [productType, setProductType] = React.useState("");
-  const [loading, setLoading] = React.useState(false);
+  const [products, setProducts] = useState([]);
+  const [productType, setProductType] = useState("");
+  const [loading, setLoading] = useState(false);
 
-  React.useEffect(() => {
+  useEffect(() => {
     setLoading(true);
     fetch(`http://makeup-api.herokuapp.com/api/v1/products.json?product_type=${productType}`)
       .then(response => response.json())
@@ -27,6 +28,9 @@ function App() {
 
   return (
     <Container>
+      <Title>
+        Makeup Search
+      </Title>
       <InputGroup className="mt-3 mb-3">
         <FormControl
           value={productType}
---
+++ src/components/Title.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const Title = styled.h1 `
+  width: 100%;
+  margin-bottom: 5px;
+  text-align: center;
+`;
+
+export default Title;
